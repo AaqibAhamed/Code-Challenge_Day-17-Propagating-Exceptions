9f2518dc6b8120530c35fb37f1313bc0ae2e464b
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-08-07 10:29:44 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_Challenge_Day_17__Propagating_Exceptions
+{
+    class Calculator
+    {
+        public int power(int x, int y)
+        {
+            if(x<0 || y<0)
+            {
+                throw new Exception("n and p should be non-negative");
+            }
+
+            else
+            {
+                return (int)Math.Pow(x, y);
+            }           
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Code_Challenge_Day_17__Propagating_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            int T = Int32.Parse(Console.ReadLine());
+
+            while(T-->0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+
+                try
+                {
+                    int ans = calculator.power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+           
+        }
+    }
+}
